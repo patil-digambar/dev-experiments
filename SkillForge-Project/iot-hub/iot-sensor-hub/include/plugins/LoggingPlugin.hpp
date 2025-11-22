cdaff63fa7aeb519d0c1ae8274cf17399282a7dd
digambar patil <digambarjpatil143@gmail.com>
2025-11-22 20:29:49 +0100

    delteing old project

--- SkillForge-Project/iot-hub/iot-sensor-hub/include/plugins/LoggingPlugin.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "plugins/IPlugin.hpp"
-class LoggingPlugin : public IPlugin {
-public:
-    std::string name() const override;
-    void initialize(MessageBus& bus) override;
-    void shutdown() override;
-    ~LoggingPlugin() override;
-};
--- SkillForge-Project/iot-hub/iot-sensor-hub/include/sensors/ISensor.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class ISensor {
-public:
-    virtual void start() = 0;
-    virtual void stop() = 0;
-    virtual ~ISensor() = default;
-};
